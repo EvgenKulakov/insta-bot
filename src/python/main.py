a73a0ac9c9bd19774896f1d25a7d1ea9a0cbf71d
Evgeniy Kulakov <000evgeniykulakov@gmail.com>
2024-03-26 01:38:16 +0300

    obser po autentification

--- src/python/main.py
+++ src/python/main.py
@@ -1,1 +1,21 @@
+import instaloader
+
+def download():
+    L = instaloader.Instaloader()
+    L.login('evgeniy-k123456789@protonmail.com', 'insta54535251')
+
+    # Укажите имя пользователя
+    username = 'maga_isma'
+
+    # Скачайте сторис
+    L.download_stories(username)
+
+    # Путь к скачанным сторис
+    stories_path = f"./{username}/stories"
+
+    # Вывод информации
+    print(f"Сторис пользователя {username} успешно скачаны в папку {stories_path}")
+
+
+download()
 
